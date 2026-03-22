e6bfa679e01ce83f9aa72e7a39d91fa07ad5813e
FlacoFavor <julianbarrera993@gmail.com>
2026-03-22 06:43:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = 'tension-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const assets = ['./', './index.html',"./estilos.css","./script.js",'./manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
