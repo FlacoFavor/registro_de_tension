3bff446b80b2dbd70266db7ce4aafe67222ed6a6
FlacoFavor <julianbarrera993@gmail.com>
2026-08-09 18:43:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -425,8 +425,8 @@ input.accept = ".csv, text/csv, text/plain, application/vnd.ms-excel";
 // ==========================================
 
 // Configura AQUÍ las dos horas exactas de tus avisos (Formato HH:MM)
-const HORARIO_MANANA = "18:38"; 
-const HORARIO_NOCHE  = "18:40";
+const HORARIO_MANANA = "18:45"; 
+const HORARIO_NOCHE  = "18:50";
 
 function solicitarPermisoNotificaciones() {
     if (!('Notification' in window)) {
